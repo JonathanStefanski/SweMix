51a54bce968c3e81e176c423e395fd152e8628ec
JonathanStefanski <jonathanstefanski@gmail.com>
2017-06-08 09:36:06 +0200

    Create song.service.ts

--- src/app/songs/song.service.ts
+++ src/app/songs/song.service.ts
@@ -10,9 +10,15 @@ import { environment } from '../../environments/environment';
 export class SongService {  
     private baseUrl = environment.apiUrl + 'api/songs';
     // private baseUrl = 'api/songs';
+    headers: Headers;
+    options: RequestOptions;
 
     constructor(private _http: Http,
-                private _auth: AuthService) { }
+                private _auth: AuthService) {
+      let bearer = this._auth.currentUser.access_token;
+      this.headers = new Headers({ 'Content-Type': 'application/json','Authorization': `Bearer ${bearer}` });
+      this.options = new RequestOptions({ headers: headers });     
+    }
 
     private extractData(response: Response) {        
         let body = response.json();
@@ -20,22 +26,16 @@ export class SongService {
     }
 
     getSongs() : Observable<Song[]> {   
-        let headers = new Headers({ 'Content-Type': 'application/json','Authorization': 'Bearer ' + this._auth.currentUser.access_token });
-        let options = new RequestOptions({ headers: headers });
-
-        return this._http.get(this.baseUrl, options)
+        return this._http.get(this.baseUrl, this.options)
             //.do((response: Response) => console.log(response))
             .map(this.extractData)
             .catch(this._handleError);
     }
 
     getSongById(id:number) : Observable<Song> {
-        let headers = new Headers({ 'Content-Type': 'application/json','Authorization': 'Bearer ' + this._auth.currentUser.access_token });
-        let options = new RequestOptions({ headers: headers });
-
-        if (id === 0) { return Observable.of(this.initializeSong()); }
+         if (id === 0) { return Observable.of(this.initializeSong()); }
         const url = `${this.baseUrl}/${id}`;
-        return this._http.get(url, options)
+        return this._http.get(url, this.options)
             //.do((response: Response) => console.log(response))
             .map(this.extractData)
             .catch(this._handleError);
@@ -50,26 +50,23 @@ export class SongService {
     }
 
     saveProduct(song: Song): Observable<Song> {        
-        let headers = new Headers({ 'Content-Type': 'application/json','Authorization': 'Bearer ' + this._auth.currentUser.access_token });
-        let options = new RequestOptions({ headers: headers });
-
         if (song.id === 0) {
-            return this.createSong(song, options);
+            return this.createSong(song);
         }
-        return this.updateSong(song, options);
+        return this.updateSong(song);
     }
 
-    private createSong(song: Song, options: RequestOptions): Observable<Song> {
+    private createSong(song: Song): Observable<Song> {
         song.id = undefined;
-        return this._http.post(this.baseUrl, song, options)
+        return this._http.post(this.baseUrl, song, this.options)
             .map(this.extractData)
             .do(data => console.log('createSong: ' + JSON.stringify(data)))
             .catch(this._handleError);
     }
 
-    private updateSong(song: Song, options: RequestOptions): Observable<Song> {
+    private updateSong(song: Song): Observable<Song> {
         const url = `${this.baseUrl}/${song.id}`;
-        return this._http.put(url, song, options)
+        return this._http.put(url, song, this.options)
             .map(() => song)
             .do(data => console.log('updateSong: ' + JSON.stringify(data)))
             .catch(this._handleError);
