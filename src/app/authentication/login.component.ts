94194c3704c6023e0f2eb3d77bd7f74bbb5d08bf
JonathanStefanski <jonathanstefanski@gmail.com>
2017-06-11 23:36:01 +0200

    Login error message and waiting icon

--- src/app/authentication/login.component.ts
+++ src/app/authentication/login.component.ts
@@ -14,16 +14,21 @@ export class LoginComponent {
     pageTitle = 'Log In';
     userName: string;
     password: string;
+    waiting: boolean;
 
     constructor(private authService: AuthService,
-                private router: Router) { }
+                private router: Router) {
+                    this.waiting = false;
+                 }
 
     login(loginForm: NgForm) {
         if (loginForm && loginForm.valid) {
             let userName = loginForm.form.value.userName;
             let password = loginForm.form.value.password;
+            this.waiting = true;
             this.authService.login(userName, password).subscribe(
                 result => {
+                    this.waiting = false;
                     if (result) {
                         if (this.authService.redirectUrl) {
                             this.router.navigateByUrl(this.authService.redirectUrl);
@@ -33,6 +38,10 @@ export class LoginComponent {
                     } else {
                         this.errorMessage = 'Please enter a user name and password.';
                     }
+                },
+                error => {
+                    this.waiting = false;
+                    this.errorMessage = 'Uw wachtwoord of gebruikersnaam is foutief.';
                 }
             );
         }
